df092a6f87bf81ddc19cc773960eec61aa297d07
adrisanchu <adrian.sanchez@constant-urban.eu>
2023-08-30 11:10:05 +0200

    #ui #home #tweets: tweet component + read back data on the front

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,8 +1,26 @@
+<script lang="ts">
+	import Tweet from '$root/components/tweet.svelte';
+	import type { TweetType } from '$root/types';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+	// load tweets from DB
+	const tweets: TweetType[] = data.tweets;
+</script>
+
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 
 <h1>Feed</h1>
+
+{#if tweets.length === 0}
+	<p>No tweets to display</p>
+{:else}
+	{#each tweets as tweet (tweet.id)}
+		<Tweet {tweet} />
+	{/each}
+{/if}
 
 <style>
 	h1 {
